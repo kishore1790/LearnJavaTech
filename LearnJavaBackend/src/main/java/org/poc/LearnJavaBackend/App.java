df235539c90f4602361dd84fabcbd689f473ce22
kishore_raju <kishore.rajuv1790@gmail.com>
2017-08-06 18:49:49 +0530

    My First Spring-Angular Project

---
+++ LearnJavaBackend/src/main/java/org/poc/LearnJavaBackend/App.java
@@ -0,0 +1,13 @@
+package org.poc.LearnJavaBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
